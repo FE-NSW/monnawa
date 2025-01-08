83bbbfa81c5d0662880db9feeb6a82302c384bd1
FE-NSW <hotdari90@gmail.com>
2025-01-08 21:09:26 +0900

    <chore>: 구글 폰트 모듈 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Roboto: true,         // 기본 Roboto 폰트
+      'Open+Sans': [300, 400, 600], // Open Sans 폰트의 다양한 굵기
+    },
+  },
 })
