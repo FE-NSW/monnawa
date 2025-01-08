f266b71ce9055e998a39edd4bc1bf6fa659eeaae
FE-NSW <hotdari90@gmail.com>
2025-01-08 20:23:45 +0900

    <refactor> 주석 추가 및 필요없는 import ref 제거

--- pages/reviews/new.vue
+++ pages/reviews/new.vue
@@ -1,15 +1,4 @@
-<template>
-  <div>
-    <strong>{{ reviewStore.getStoreName }}</strong>
-    <p>{{ reviewStore.getEpName }}</p>
-    <p>탈출에 성공 하셨나요?</p>
-    <button type="button" @click="escaping('y')">탈출 성공~!</button>
-    <button type="button" @click="escaping('n')">아깝다....실패...</button>
-  </div>
-</template>
-
 <script setup>
-import { ref } from 'vue';
 import { useReviewStore } from '~/stores/reviews/review'
 import { useRouter } from 'vue-router'
 
@@ -19,6 +8,7 @@ const reviewStore = useReviewStore();
 // 라우터 설정
 const router = useRouter();
 
+// 리뷰할 가게 정보 불러오기
 onMounted(()=>{
   const aa = reviewStore.getReviewInfol;
 })
@@ -32,3 +22,13 @@ const escaping = (val) => {
   }
 };
 </script>
+
+<template>
+  <div>
+    <strong>{{ reviewStore.getStoreName }}</strong>
+    <p>{{ reviewStore.getEpName }}</p>
+    <p>탈출에 성공 하셨나요?</p>
+    <button type="button" @click="escaping('y')">탈출 성공~!</button>
+    <button type="button" @click="escaping('n')">아깝다....실패...</button>
+  </div>
+</template>
