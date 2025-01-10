8e465c4b68a55aef869ba81209ceb91a4097b8e1
FE-NSW <hotdari90@gmail.com>
2025-01-10 10:41:37 +0900

    <fix> : 프론트 페이지 충돌 수정

---
+++ services/reviews/reviewService.ts
@@ -0,0 +1,20 @@
+import { useNuxtApp } from '#app';
+
+export const reviewService = () => {
+  const { $axios } = useNuxtApp();
+
+  // 리뷰 데이터를 가져오는 API 호출 함수
+  const getClearGame = async () => {
+    try {
+      const { data } = await $axios.get('/json/clearGame.json'); // API 경로로 변경
+      return data;
+    } catch (error) {
+      console.error('Error fetching reviews:', error);
+      throw error; // 오류가 발생한 경우, 상위에서 처리할 수 있도록 예외 던지기
+    }
+  };
+
+  return {
+    getClearGame
+  };
+};
