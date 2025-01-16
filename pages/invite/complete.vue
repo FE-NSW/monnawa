0367b216e44ef7b47672bda3b74b0d9839019736
FE-NSW <hotdari90@gmail.com>
2025-01-16 15:40:28 +0900

    <docs>:필요한 alert page 추가

---
+++ pages/invite/complete.vue
@@ -0,0 +1,22 @@
+<script setup>
+import alertPage from '~/components/alertPage.vue'
+
+const handleAction = () => {
+  console.log('버튼액션')
+}
+</script>
+
+<template>
+  <alertPage 
+    :title= "'초대하기 완료'"
+    :description="'즐거운 방탈출 하세요!'"
+    :buttonText="'작성한 초대장 보기'"
+    :buttonAction="handleAction"
+  />
+</template>
+
+<style scoped>
+</style>
+
+
+
---
+++ pages/reviews/complete.vue
@@ -0,0 +1,22 @@
+<script setup>
+import alertPage from '~/components/alertPage.vue'
+
+const handleAction = () => {
+  console.log('버튼액션')
+}
+</script>
+
+<template>
+  <alertPage 
+    :title= "'리뷰 작성 완료'"
+    :description="'소중한 추억을 남기셨네요^^'"
+    :buttonText="'오늘의 MVP투표'"
+    :buttonAction="handleAction"
+  />
+</template>
+
+<style scoped>
+</style>
+
+
+
